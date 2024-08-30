b4069695bff720b2367123d59df4bf32ad78694c
Fausto Puga <fauspuga@gmail.com>
2024-08-29 21:54:16 -0600

    06-Generic list and detail views, 07-Sessions, 08-Authentication and permissions

--- catalog/models.py
+++ catalog/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-
-# Create your models here.
 from django.urls import reverse # Used in get_absolute_url() to get URL for specified ID
-
 from django.db.models import UniqueConstraint # Constrains fields to unique values
 from django.db.models.functions import Lower # Returns lower cased value of field
+from django.conf import settings
 import uuid # Required for unique book instances
+from datetime import date
+
 
 class Genre(models.Model):
     """Model representing a book genre."""
@@ -53,6 +53,9 @@ class Book(models.Model):
     
     language = models.ForeignKey('Language', on_delete=models.RESTRICT, help_text='Select a language for this book', null=True)
 
+    class Meta:
+        ordering = ['title']
+
     def __str__(self):
         """String for representing the Model object."""
         return self.title
@@ -91,8 +94,16 @@ class BookInstance(models.Model):
         help_text='Book availability',
     )
 
+    borrower = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True)
+
+    @property
+    def is_overdue(self):
+        """Determines if the book is overdue based on due date and current date."""
+        return bool(self.due_back and date.today() > self.due_back)
+
     class Meta:
         ordering = ['due_back']
+        permissions = (("can_mark_returned", "Set book as returned"),("can_view_loaned_books","View the loaned books"))
 
     def __str__(self):
         """String for representing the Model object."""
@@ -115,7 +126,6 @@ class Author(models.Model):
     def __str__(self):
         """String for representing the Model object."""
         return f'{self.last_name}, {self.first_name}'
-
 
 class Language(models.Model):
     """Model representing a language"""
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Book, Author, BookInstance
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
 # Create your views here.
 def index(request):
@@ -16,6 +18,10 @@ def index(request):
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
 
+    num_visits = request.session.get('num_visits',0)
+    num_visits += 1
+    request.session['num_visits'] = num_visits
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
@@ -23,7 +29,52 @@ def index(request):
         'num_authors': num_authors,
         'num_books_novels': num_books_novels,
         'num_books_night': num_books_night,
+        'num_visits': num_visits,
     }
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+class LoanedBooksByUserListView(LoginRequiredMixin,generic.ListView):
+    """Generic class-based view listing books on loan to current user."""
+    model = BookInstance
+    template_name = 'catalog/bookinstance_list_borrowed_user.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return (
+            BookInstance.objects.filter(borrower=self.request.user)
+            .filter(status__exact='o')
+            .order_by('due_back')
+        )
+
+class LoanedBooksListView(PermissionRequiredMixin,generic.ListView):
+    """Generic class-based view listing books on loan to current user."""
+    permission_required = 'catalog.can_view_loaned_books'
+
+    model = BookInstance
+    template_name = 'catalog/bookinstance_list_loaned.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return (
+            BookInstance.objects.filter(status__exact='o')
+            .order_by('due_back')
+        )
+
+class BookListView(generic.ListView):
+    model = Book
+    queryset = Book.objects.all()
+    paginate_by = 5
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+    queryset = Author.objects.all()
+    paginate_by = 5
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+
